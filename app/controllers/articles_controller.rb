1080f2a65c9f4365e6ea8dd3c0708ed44d707201
Aiko <tysaiw@gmail.com>
2023-07-13 17:17:55 +0800

    add article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,35 @@ class ArticlesController < ApplicationController
     def articles
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to "/articles", notice: "文章新增成功"
+        else
+            #拿new.html.erb
+            render:new
+        end
+
+        
+        # flash[notice] = "文章新增成功"
+        # flash[:alert] = ""
+        #ORM object relation mapping
+        #strong parameter
+        # clean_params = params.require(:article).permit(:title, :content)
+        # article = Article.new(clean_params)
+        # article.save
+
+        # article = Article.new(title: params[:cc][:title],
+        #                         content: params[:cc][:content])
+        #                         article save
+    end
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ get"/", to: "articles#index"
 get"/about", to: "pages#about"
 get"/articles", to: "articles#index"
 get"/articles/new", to: "articles#new"
+post"/articles", to: "articles#create"
 
 end
